2ba6a71e792f2629823ebec7adbe884cf9645c39
Michael Holloway <michael.a.holloway@outlook.com>
2020-07-11 19:03:44 -0700

    Initial chatbot

---
+++ src/index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+var ComfyJS = require("comfy.js");
+
+ComfyJS.onCommand = ( user, command, message, flags, extra ) => {
+    
+  if( command === "test" ) {
+    ComfyJS.Say( "replying to !test" );
+  }
+}
+
+ComfyJS.onChat = ( user, message, flags, self, extra ) => {
+  if( flags.broadcaster && command === "test" ) {
+    if( extra.sinceLastCommand.any < 100 ) {
+      console.log(
+        `The last '!test' command by any user was sent less than 100 ms ago`
+      );            
+    }
+
+    if( extra.sinceLastCommand.user < 100 ) {
+      console.log(
+        `The last '!test' command by this specific user (as denoted by the 'user' parameter) was sent less than 100 ms ago`
+      );            
+    }
+  }
+}
+
+ComfyJS.Init( process.env.TWITCHUSER, process.env.OAUTH );
