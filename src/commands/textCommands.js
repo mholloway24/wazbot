819a7d2ae2a70ba854c8fb6523fa3c7d5a6d4654
Michael Holloway <michael.a.holloway@outlook.com>
2020-10-03 17:51:22 -0700

    Add Github command

--- src/commands/textCommands.js
+++ src/commands/textCommands.js
@@ -1,12 +1,20 @@
 
-var textCommands = [    
+var textCommands = [
     {
         command: 'help',
-        text: 'You can use the following commands in chat: !test !help'
+        text: 'You can use the following commands in chat: !help !about !github !discord'
     },
     {
-        command: 'test',
-        text: 'replying to !test'
+        command: 'github',
+        text: 'You can see WazabyDev\'s github at https://github.com/mholloway24'
+    },
+    {
+        command: 'discord',
+        text: 'Join our Discord server at https://discord.gg/Ba8NKM'
+    },
+    {
+        command: 'about',
+        text: 'Wanna know about WazabyDev? Just ask.'
     }
 ];
 
